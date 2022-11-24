830d793a09ac5d6d605bda139906a02ad348c334
wuhibe <wuhibetamire710@gmail.com>
2022-11-24 15:24:10 +0300

    create middleware to log request params

--- myApp.js
+++ myApp.js
@@ -4,7 +4,10 @@ let app = express();
 
 console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
-
+app.use(function(req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
