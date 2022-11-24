f103850968287cd2f54441d74d7ad742bbf5f788
wuhibe <wuhibetamire710@gmail.com>
2022-11-24 15:29:15 +0300

    create middleware to log request params

--- myApp.js
+++ myApp.js
@@ -1,13 +1,12 @@
 let express = require("express");
-require("dotenv").config();
 let app = express();
 
 console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
-app.use(function(req, res, next) {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
-})
+app.use(function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
