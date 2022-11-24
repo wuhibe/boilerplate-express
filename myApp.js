6993871fc2115aaab01645a7ce680f13e848b01e
wuhibe <wuhibetamire710@gmail.com>
2022-11-24 14:33:33 +0300

    json response to an endpoint

--- myApp.js
+++ myApp.js
@@ -2,13 +2,13 @@ let express = require("express");
 let app = express();
 
 console.log("Hello World");
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
-
-
-
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
+});
 
 module.exports = app;
