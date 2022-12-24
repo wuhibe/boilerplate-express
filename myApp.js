aa4bff98a3a9dfd38dfd7af5986abd4f6bb9a525
Wuhibe Tamire <wuhibetamire710@gmail.com>
2022-12-24 09:13:51 +0000

    add route /now to endpoints

--- myApp.js
+++ myApp.js
@@ -11,6 +11,13 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.send({time: req.time});
+});
+
 app.get("/json", function (req, res) {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === "uppercase")
