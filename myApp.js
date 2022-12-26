2fff55908c8302727f046c569d2b0b2397ac2abd
Wuhibe Tamire <wuhibetamire710@gmail.com>
2022-12-26 06:24:59 +0000

    modify post request on /name route

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require("express");
 let app = express();
+let bodyParser = require('body-parser');
 
 console.log("Hello World");
+app.use(bodyParser.urlencoded({extended: false}));
 app.use("/public", express.static(__dirname + "/public"));
 app.use(function (req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
@@ -11,7 +13,7 @@ app.get('/name', (req, res) => {
     res.json({name: `${req.query.first} ${req.query.last}`})
 });
 app.post('/name', (req, res) => {
-    res.json({name: `${req.query.first} ${req.query.last}`})
+    res.json({name: `${req.body.first} ${req.body.last}`})
 });
 
 app.get("/", function (req, res) {
